f75277e291c2f4abc2158a93d0857af4360b607b
Reinaldo Silva <reinaldoblack@hotmail.com>
2024-04-22 06:37:01 -0300

    Update forArrays.java

--- src/forArrays.java
+++ src/forArrays.java
@@ -1,2 +1,19 @@
 public class forArrays {
+    public static void main(String[] args){
+
+    // Definindo um arrays de numeros inteiros
+    int[] numeros = {10, 20, 30, 40, 50};
+
+    // variavel para armazenar a soma dos elementos do array
+    int soma = 0;
+
+    // loop for para iteirar sobre os elementos
+    for (int i = 0; i < numeros.length; i++){
+        // Adicionando o valor do elemento atual ao total
+        soma += numeros[i];
+    }
+
+    // Calculando a media dos elementos
+
+    }
 }
